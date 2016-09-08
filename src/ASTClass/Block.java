e6e3ea8ae0494728fd63c3f17fa45dadd7a73fe2
Ezequiel Depetris <ezedepetris@gmail.com>
2016-09-08 20:36:09 -0300

    add initial block class

--- src/ASTClass/Block.java
+++ src/ASTClass/Block.java
@@ -1,5 +1,18 @@
 package ASTClass.Block;
 
 class Block{
+	private LinkedList<FieldDecl> field_decl;
+	private LinkedList<Statement> statement;
+
+	public Block(LinkedList<FieldDecl> list_fiel_decl){
+		this.field_decl = list_fiel_decl ;
+	}
+
+	public Block(LinkedList<Statement> list_statement){
+		this.statement = list_statement;
+	}
+
+	public Block(){
+	}
 	
 }
