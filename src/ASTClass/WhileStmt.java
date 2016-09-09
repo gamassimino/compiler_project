7f97774b5cc2562a3eace006cee3de169a19f301
Ezequiel Depetris <ezedepetris@gmail.com>
2016-09-08 23:03:35 -0300

    for and while classgst

---
+++ src/ASTClass/WhileStmt.java
@@ -0,0 +1,43 @@
+package ASTClass.WhileStmt;
+
+import ir.ASTVisitor;
+WHILE expr:exp statement:s {: RESULT = new Statement(exp, s) ;:}
+public class WhileStmt extends Statement {
+	private Expression condition;
+	private Statement statement;
+	
+	public WhileStmt(Expression cond, Statement a_statement) {
+		this.condition = cond;
+		this.statement = a_statement;
+	}
+
+	public Expression getCondition() {
+		return condition;
+	}
+
+	public void setCondition(Expression condition) {
+		this.condition = condition;
+	}
+
+	public Statement getStatement() {
+		return statement;
+	}
+
+	public void setStatement(Statement a_statement) {
+		this.statement = a_statement;
+	}
+	
+	@Override
+	public String toString() {
+		String rtn = "while " + condition + '\n';
+		
+		rtn += statement.toString;
+		
+		return rtn;
+	}
+
+	@Override
+	public <T> T accept(ASTVisitor<T> v) {
+		return v.visit(this);
+	}
+}
